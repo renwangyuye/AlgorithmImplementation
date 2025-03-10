3f4062ae0fde21999ad71f755268b4f2d7a685ef
doncic <143199680+renwangyuye@users.noreply.github.com>
2025-03-10 17:18:06 +0800

    merge

---
+++ Merge/merge.java
@@ -0,0 +1,79 @@
+package class022;
+
+// 这是输入输出处理效率很高的写法
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+public class Code01_SmallSum {
+
+    public static int MAXN = 100001;
+
+    public static int[] arr = new int[MAXN];
+
+    public static int[] help = new int[MAXN];
+
+    public static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while (in.nextToken() != StreamTokenizer.TT_EOF) {
+            n = (int) in.nval;
+            for (int i = 0; i < n; i++) {
+                in.nextToken();
+                arr[i] = (int) in.nval;
+            }
+            out.println(smallSum(0, n - 1));
+        }
+        out.flush();
+        out.close();
+    }
+
+    // 结果比较大，用int会溢出的，所以返回long类型
+    // 特别注意溢出这个点，笔试常见坑
+    // 时间复杂度O(n * logn)
+    public static long mergeSort(int l, int r) {
+        if (l == r) {
+            return 0;
+        }
+        int m = (l + r) / 2;
+        return mergeSort(l, m) + mergeSort(m + 1, r) + merge(l, m, r);
+    }
+
+    // 左侧有序、右侧有序，让左右两侧整体有序
+    // arr[l...m] arr[m+1...r]
+    public static long merge(int l, int m, int r) {
+        // 统计部分
+        long ans = 0;
+        for (int j = m + 1, i = l; j <= r; j++) {
+            while (j <= r && 判断条件) {
+                j++;
+            }
+            ans += j - (m + 1);
+        }
+        // 正常merge
+        int i = l;
+        int a = l;
+        int b = m + 1;
+        while (a <= m && b <= r) {
+            help[i++] = arr[a] <= arr[b] ? arr[a++] : arr[b++];
+        }
+        while (a <= m) {
+            help[i++] = arr[a++];
+        }
+        while (b <= r) {
+            help[i++] = arr[b++];
+        }
+        for (i = l; i <= r; i++) {
+            arr[i] = help[i];
+        }
+        return ans;
+    }
+
+}
